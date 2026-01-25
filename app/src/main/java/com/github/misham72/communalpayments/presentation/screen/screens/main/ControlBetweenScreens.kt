1f6017981a8db6477b26c1461cc85204aa45bced
Mikhail <magolyasov@yandex.ru>
2026-01-26 01:57:47 +0300

    изменил архитектуру

--- app/src/main/java/com/github/misham72/communalpayments/presentation/screen/screens/main/ControlBetweenScreens.kt
+++ app/src/main/java/com/github/misham72/communalpayments/presentation/screen/screens/main/ControlBetweenScreens.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.material3.Button
@@ -23,6 +24,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
@@ -108,13 +110,13 @@ fun ControlBetweenScreens() {
                 Box(modifier = Modifier.weight(1f)) {
                     services[selectedService].screen()
                 }
-
                 Image(
                     painter = painterResource(R.drawable.night),
                     contentDescription = stringResource(R.string.summer_night),
+                    contentScale = ContentScale.FillWidth, // Растягивает по ширине, сохраняя пропорции
                     modifier = Modifier
                         .fillMaxWidth()
-                        .weight(1f)
+                        .height(250.dp)
                 )
 
                 // Кнопка с кастомным цветом (если хотите)
