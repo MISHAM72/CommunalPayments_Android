572138bdcb9e8c7f1ede9e8c45f400b5a7411e0c
Mikhail <magolyasov@yandex.ru>
2026-04-06 04:03:17 +0300

    "Улучшил исправиленный калькулятор месячных периодов, bump version to 2.0.3"

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.github.misham72.communalpayments"
         minSdk = 24
         targetSdk = 36
-        versionCode = 4
-        versionName = "2.0.2"
+        versionCode = 5
+        versionName = "2.0.3"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
     }
