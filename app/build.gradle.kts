9511d049f5d473cd004842f69312679a85d14c71
Mikhail <magolyasov@yandex.ru>
2026-04-13 14:30:51 +0300

    Исправлен .gitignore и обновлена версия до 2.2.4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
         applicationId = "com.github.misham72.communalpayments"
         minSdk = 24
         targetSdk = 36
-        versionCode = 7
-        versionName = "2.1.4"
+        versionCode = 8
+        versionName = "2.2.4"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
     buildTypes {
