db007dcb5814e76498487e98ca0363ba62e46366
Mikhail <magolyasov@yandex.ru>
2026-07-02 21:34:39 +0300

    Финальная очистка кода перед v2.5.5 с README.md

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -30,8 +30,8 @@ android {
         applicationId = "com.github.misham72.communalpayments"
         minSdk = 26
         targetSdk = 37
-        versionCode = 16
-        versionName = "2.5.4"
+        versionCode = 17
+        versionName = "2.5.5"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
         // Чтение API-ключа из local.properties
